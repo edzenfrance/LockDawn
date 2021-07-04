5c63503b02956a016b163a39bcffc9697688b56f
edzenfrance <68797488+edzenfrance@users.noreply.github.com>
2021-07-04 15:58:27 +0800

    Testing character selection

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -27,6 +27,6 @@ public class CharacterSelection : MonoBehaviour
 	public void StartGame()
 	{
 		PlayerPrefs.SetInt("selectedCharacter", selectedCharacter);
-		SceneManager.LoadScene(1, LoadSceneMode.Single);
+		SceneManager.LoadScene(5, LoadSceneMode.Single);
 	}
 }
