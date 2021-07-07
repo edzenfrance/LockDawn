b328774c220e967659628c06c070a39bc8845a1d
edzenfrance <68797488+edzenfrance@users.noreply.github.com>
2021-07-07 16:19:16 +0800

    Changed fps speed to 0.5

--- Assets/Scripts/FPSCounter.cs
+++ Assets/Scripts/FPSCounter.cs
@@ -27,7 +27,7 @@ public class FPSCounter : MonoBehaviour
 
     IEnumerator refreshDelay()
     {
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(0.5f);
         refresh = true;
     }
 
