b328b0c9cb4e79271d2d6d5e4b596b4aa3bf5639
edzenfrance <68797488+edzenfrance@users.noreply.github.com>
2021-07-07 14:57:35 +0800

    Renamed variables in Cinemachine Follow Player script

--- Assets/Scripts/Stage1/CinemachineFollowPlayer.cs
+++ Assets/Scripts/Stage1/CinemachineFollowPlayer.cs
@@ -4,24 +4,27 @@ using Cinemachine;
 
 public class CinemachineFollowPlayer : MonoBehaviour
 {
-    public GameObject tPlayer;
-    public Transform tFollowTarget;
     [SerializeField]
     private CinemachineVirtualCamera vcam;
     [SerializeField]
     private Cinemachine3rdPersonFollow vcamThirdPersonFollow;
+
+    public GameObject vcamPlayerCameraRoot;
+    public Transform vcamFollow;
+
+
 
     private void Awake()
     {
         vcam = GameObject.Find("PlayerFollowCamera").GetComponent<CinemachineVirtualCamera>();
         vcamThirdPersonFollow = vcam.GetCinemachineComponent<Cinemachine3rdPersonFollow>();
 
-        if (tPlayer == null)
+        if (vcamPlayerCameraRoot == null)
         {
-            tPlayer = GameObject.FindWithTag("CinemachineTarget");
+           vcamPlayerCameraRoot = GameObject.FindWithTag("CinemachineTarget");
         }
-        tFollowTarget = tPlayer.transform;
-        vcam.Follow = tFollowTarget;
-        //vcam.LookAt = tFollowTarget;
+        vcamFollow = vcamPlayerCameraRoot.transform;
+        vcam.Follow = vcamFollow;
+        //vcam.LookAt = vcamFollowTarget;
     }
 }
