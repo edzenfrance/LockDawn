9dfa2cc650c6848dcd6f5ca624b58085d87512e4
edzenfrance <68797488+edzenfrance@users.noreply.github.com>
2021-07-24 23:07:23 +0800

    Graphics quality can now be saved in playerprefs

--- Assets/Scripts/MainMenu/GraphicsToggleSelect.cs
+++ Assets/Scripts/MainMenu/GraphicsToggleSelect.cs
@@ -2,14 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class GraphicsToggleSelect : MonoBehaviour
 {
     [SerializeField] private ToggleGroup toggleGroup;
+    [SerializeField] private Toggle toggleQualityLow, toggleQualityMedium, toggleQualityHigh;
+    [SerializeField] private int setGraphicsQuality;
 
     private void Awake()
     {
         toggleGroup = GetComponent<ToggleGroup>();
+    }
+
+    void Start()
+    {
+        setGraphicsQuality = PlayerPrefs.GetInt("LDGraphicsQuality");
+        if (setGraphicsQuality == 1)
+        {
+            Debug.Log("<color=white>GraphicsToggleSelect</color> - Set Quality: Low");
+            toggleQualityLow.isOn = true;
+        }
+        if (setGraphicsQuality == 2)
+        {
+            Debug.Log("<color=white>GraphicsToggleSelect</color> - Set Quality: Medium");
+            toggleQualityMedium.isOn = true;
+        }
+        if (setGraphicsQuality == 3)
+        {
+            Debug.Log("<color=white>GraphicsToggleSelect</color> - Set Quality: High");
+            toggleQualityHigh.isOn = true;
+        }
+        else
+        {
+            Debug.Log("<color=white>GraphicsToggleSelect</color> - No Set Quality, Auto Set Quality: Medium");
+            toggleQualityMedium.isOn = true;
+        }
     }
 
     public void LogSelectedToggle()
@@ -19,15 +47,18 @@ public class GraphicsToggleSelect : MonoBehaviour
         {
             if (toggle.name == "Low")
             {
-                Debug.Log("GraphicsController - Quality: Low");
+                Debug.Log("<color=white>GraphicsToggleSelect</color> - Selected Quality: Low");
+                PlayerPrefs.SetInt("LDGraphicsQuality", 1);
             }
             if (toggle.name == "Medium")
             {
-                Debug.Log("GraphicsController - Quality: Medium");
+                Debug.Log("<color=white>GraphicsToggleSelect</color> - Selected Quality: Medium");
+                PlayerPrefs.SetInt("LDGraphicsQuality", 2);
             }
             if (toggle.name == "High")
             {
-                Debug.Log("GraphicsController - Quality: High");
+                Debug.Log("<color=white>GraphicsToggleSelect</color> - Selected Quality: High");
+                PlayerPrefs.SetInt("LDGraphicsQuality", 3);
             }
         }
 
