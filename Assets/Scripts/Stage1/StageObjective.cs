9175a62a6af74a55dfc12fe13964a9bcc452c53c
edzenfrance <68797488+edzenfrance@users.noreply.github.com>
2021-11-11 17:37:43 +0800

    Renamed Stage Mission script to Stage Objective
    Rename ObjectiveScreen object to StageObjective

--- Assets/Scripts/Stage1/StageObjective.cs
+++ Assets/Scripts/Stage1/StageObjective.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StageMission : MonoBehaviour
+public class StageObjective : MonoBehaviour
 {
 
     [SerializeField] private AudioManager audioManager;
