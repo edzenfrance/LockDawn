d0d2d93d9c97e6af2a844c502745de5c34a0d209
edzenfrance <68797488+edzenfrance@users.noreply.github.com>
2021-11-13 20:51:19 +0800

    Changed Load Character script
    Renamed Load Character script to Load Character NPC
    Deleted Load NPC script and moved the codes to Load Character NPC
    Moved Load Character NPC script to Stage 1 folder

--- Assets/Scripts/Stage1/LoadCharacterNPC.cs
+++ Assets/Scripts/Stage1/LoadCharacterNPC.cs
@@ -1,19 +1,29 @@
 ﻿using UnityEngine;
 using TMPro;
 
-public class LoadCharacter : MonoBehaviour
+public class LoadCharacterNPC : MonoBehaviour
 {
+    [SerializeField] private TMP_Text selectedCharacterText;
     [SerializeField] private GameObject[] characterPrefabs;
     [SerializeField] private Transform[] spawnPoint;
-    [SerializeField] private TMP_Text selectedCharacterText;
-
+    [SerializeField] private GameObject[] NPCDifficulty;
     void Awake()
     {
+        // Character
         int selectedCharacter = PlayerPrefs.GetInt("Current Character", 0);
         int currentStage = PlayerPrefs.GetInt("Current Stage", 1);
         GameObject prefab = characterPrefabs[selectedCharacter];
         GameObject clone = Instantiate(prefab, spawnPoint[currentStage].position, Quaternion.identity);
         Debug.Log("<color=white>LoadCharacter</color> - Enabled Character: " + prefab.name);
         selectedCharacterText.text = "Player: " + prefab.name;
+
+        // NPC
+        int npcDiff = PlayerPrefs.GetInt("Game Difficulty", 2);
+        NPCDifficulty[0].SetActive(true);
+        NPCDifficulty[1].SetActive(false);
+        NPCDifficulty[2].SetActive(false);
+        if (npcDiff == 1) NPCDifficulty[0].SetActive(true);
+        if (npcDiff == 2) NPCDifficulty[1].SetActive(true);
+        if (npcDiff == 3) NPCDifficulty[2].SetActive(true);
     }
 }
